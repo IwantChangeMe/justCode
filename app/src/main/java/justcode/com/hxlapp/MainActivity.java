c1108cbd4be59e0cfeba4b02a39bc3665aea9a3e
haoxingling <hxl4321>
2018-03-19 12:07:28 +0800

    集成bugly

--- app/src/main/java/justcode/com/hxlapp/MainActivity.java
+++ app/src/main/java/justcode/com/hxlapp/MainActivity.java
@@ -1,7 +1,7 @@
 package justcode.com.hxlapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
--- common/src/main/java/justcode/com/common/BaseApplication.java
+++ common/src/main/java/justcode/com/common/BaseApplication.java
@@ -2,10 +2,64 @@ package justcode.com.common;
 
 
 import android.app.Application;
+import android.content.Context;
+import android.text.TextUtils;
+
+import com.tencent.bugly.crashreport.CrashReport;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class BaseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        initBugly();
+    }
+
+    private void initBugly() {
+        Context context = getApplicationContext();
+        // 获取当前包名
+        String packageName = context.getPackageName();
+        // 获取当前进程名
+        String processName = getProcessName(android.os.Process.myPid());
+        // 设置是否为上报进程
+        CrashReport.UserStrategy strategy = new CrashReport.UserStrategy(context);
+        strategy.setUploadProcess(processName == null || processName.equals(packageName));
+        /**
+         * 建议在测试阶段建议设置成true, 输出详细的Bugly SDK的Log, 每一条Crash都会被立即上报, 自定义日志将会在Logcat中输出
+         * 发布时设置为false
+         */
+        CrashReport.initCrashReport(context, "c8a265acbb", true, strategy);
+    }
+
+    /**
+     * 获取进程号对应的进程名
+     *
+     * @param pid 进程号
+     * @return 进程名
+     */
+    private static String getProcessName(int pid) {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader("/proc/" + pid + "/cmdline"));
+            String processName = reader.readLine();
+            if (!TextUtils.isEmpty(processName)) {
+                processName = processName.trim();
+            }
+            return processName;
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        }
+        return null;
     }
 }
